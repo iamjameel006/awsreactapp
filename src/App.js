5872ec9a51507529811a25595d87bc96890a564a
iamjameel006 <117569030+iamjameel006@users.noreply.github.com>
2024-01-05 11:21:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React Web
+          Learning React Web Development 
         </a>
       </header>
     </div>
